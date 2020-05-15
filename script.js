cb53918a725fb5e71139d7631f408efb95946d55
ikngoo <ikngoo@users.noreply.github.com>
2020-05-15 19:37:30 +0800

    ctcount

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var place_data=[
   {
    tag: "taipei_city",
    place: "臺北市",
-   low: 2280,
+   low: 2817,
    high: "  letters",
    weather: "img/wordcloud_臺北市.png"
   },
@@ -10,7 +10,7 @@ var place_data=[
   {
    tag: "new_taipei_city",
    place: "新北市",
-   low: 2769,
+   low: 3365,
    high: "  letters",
    weather: "img/wordcloud_新北市.png"
   },
@@ -18,7 +18,7 @@ var place_data=[
   {
    tag: "taichung_city",
    place: "臺中市",
-   low: 1711,
+   low: 2126,
    high: "  letters",
    weather: "img/wordcloud_臺中市.png"
   },
@@ -26,7 +26,7 @@ var place_data=[
   {
    tag: "tainan_city",
    place: "臺南市",
-   low: 857,
+   low: 1049,
    high: "  letters",
    weather: "img/wordcloud_臺南市.png"
   },
@@ -34,7 +34,7 @@ var place_data=[
   {
    tag: "kaohsiung_city",
    place: "高雄市",
-   low: 1640,
+   low: 2108,
    high: "  letters",
    weather: "img/wordcloud_高雄市.png"
   },
@@ -42,7 +42,7 @@ var place_data=[
   {
    tag: "keelung_city",
    place: "基隆市",
-   low: 400,
+   low: 452,
    high: "  letters",
    weather: "img/wordcloud_基隆市.png"
   },
@@ -50,7 +50,7 @@ var place_data=[
   {
    tag: "taoyuan_county",
    place: "桃園市",
-   low: 1172,
+   low: 1417,
    high: "  letters",
    weather: "img/wordcloud_桃園市.png"
   },
@@ -58,7 +58,7 @@ var place_data=[
   {
    tag: "hsinchu_city",
    place: "新竹市",
-   low: 321,
+   low: 392,
    high: "  letters",
    weather: "img/wordcloud_新竹市.png"
   },
@@ -66,7 +66,7 @@ var place_data=[
   {
    tag: "hsinchu_county",
    place: "新竹縣",
-   low: 353,
+   low: 407,
    high: "  letters",
    weather: "img/wordcloud_新竹縣.png"
   },
@@ -74,7 +74,7 @@ var place_data=[
   {
    tag: "miaoli_county",
    place: "苗栗縣",
-   low: 245,
+   low: 295,
    high: "  letters",
    weather: "img/wordcloud_苗栗縣.png"
   },
@@ -82,7 +82,7 @@ var place_data=[
   {
    tag: "changhua_county",
    place: "彰化縣",
-   low: 581,
+   low: 778,
    high: "  letters",
    weather: "img/wordcloud_彰化縣.png"
   },
@@ -90,7 +90,7 @@ var place_data=[
   {
    tag: "nantou_county",
    place: "南投縣",
-   low: 236,
+   low: 274,
    high: "  letters",
    weather: "img/wordcloud_南投縣.png"
   },
@@ -98,7 +98,7 @@ var place_data=[
   {
    tag: "yunlin_county",
    place: "雲林縣",
-   low: 186,
+   low: 237,
    high: "  letters",
    weather: "img/wordcloud_雲林縣.png"
   },
@@ -106,7 +106,7 @@ var place_data=[
   {
    tag: "chiayi_city",
    place: "嘉義市",
-   low: 244,
+   low: 286,
    high: "  letters",
    weather: "img/wordcloud_嘉義市.png"
   },
@@ -114,7 +114,7 @@ var place_data=[
   {
    tag: "chiayi_county",
    place: "嘉義縣",
-   low: 168,
+   low: 207,
    high: "  letters",
    weather: "img/wordcloud_嘉義縣.png"
   },
@@ -122,7 +122,7 @@ var place_data=[
   {
    tag: "pingtung_county",
    place: "屏東縣",
-   low: 262,
+   low: 436,
    high: "  letters",
    weather: "img/wordcloud_屏東縣.png"
   },
@@ -130,7 +130,7 @@ var place_data=[
   {
    tag: "yilan_county",
    place: "宜蘭縣",
-   low: 183,
+   low: 219,
    high: "  letters",
    weather: "img/wordcloud_宜蘭縣.png"
   },
@@ -138,7 +138,7 @@ var place_data=[
   {
    tag: "hualien_county",
    place: "花蓮縣",
-   low: 153,
+   low: 194,
    high: "  letters",
    weather: "img/wordcloud_花蓮縣.png"
   },
@@ -146,7 +146,7 @@ var place_data=[
   {
    tag: "taitung_county",
    place: "臺東縣",
-   low: 66,
+   low: 88,
    high: "  letters",
    weather: "img/wordcloud_臺東縣.png"
   },
@@ -154,7 +154,7 @@ var place_data=[
   {
    tag: "penghu_county",
    place: "澎湖縣",
-   low: 19,
+   low: 28,
    high: "  letters",
    weather: "img/wordcloud_澎湖縣.png"
   },
@@ -162,7 +162,7 @@ var place_data=[
   {
    tag: "kinmen_county",
    place: "金門縣",
-   low: 16,
+   low: 21,
    high: "  letters",
    weather: "img/wordcloud_金門縣.png"
   },
@@ -170,7 +170,7 @@ var place_data=[
   {
    tag: "lienchiang_county",
    place: "連江縣",
-   low: 6,
+   low: 7,
    high: "  letters",
    weather: "img/wordcloud_連江縣.png"
   },
